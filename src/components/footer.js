3d0d063e0e50e5b2d161b36477a9e7a19ebefd90
Kyle Gill <kylerobertgill@gmail.com>
2019-03-19 13:52:24 -0600

    simple footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,10 +10,21 @@ const Footer = ({ siteTitle }) => (
       backgroundColor: COLORS.lightGray,
     }}
   >
-    <div>Contact</div>
-    <hr />© {new Date().getFullYear()}
-    {` `}
-    {siteTitle}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "1rem 2rem",
+        fontSize: ".85rem",
+      }}
+    >
+      <div>Contact Us</div>
+      <div style={{ color: COLORS.mediumGray }}>
+        © {new Date().getFullYear()}
+        {` `}
+        {siteTitle}
+      </div>
+    </div>
   </footer>
 )
 
--- src/styles/constants.js
+++ src/styles/constants.js
@@ -1,16 +1,13 @@
 export const COLORS = {
-  lightWhite: "#F4F4F0",
-  semiWhite: "rgba(255,255,240,0.25)",
-  lightBlue: "#50CAF9",
-  blue: "#5390F0",
-  darkBlue: "#396CB7",
-  gray: "#777",
-  lightGray: "#f3f5f6",
-  black: "#494a66",
-  gold: "#ffbd0a",
-  semiGold: "rgba(255,189,10,0.5)",
+  lightWhite: "#FBFDFF",
+  lightGray: "#F3F5F6",
+  mediumGray: "#a0a4a6",
+  babyBlue: "#50CAF9",
+  blue: "#5368f1",
 }
 
-export const GRADIENT = `linear-gradient(to top right, #50CAF9, #5368f1)`
+export const GRADIENT = `linear-gradient(to top right, ${COLORS.babyBlue}, ${
+  COLORS.blue
+})`
 
 export const BORDER_RADIUS = "10px"
