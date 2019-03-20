ed8694a8ed0b0c516c2c1de5f4af93adbd49190b
Kyle Gill <kylerobertgill@gmail.com>
2019-03-19 22:56:18 -0600

    fixed capitalization

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
-import Content from "../components/Content"
+import Content from "../components/content"
 
 const IndexPage = () => (
   <Layout>
